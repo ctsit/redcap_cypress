f64daa7d018508e9c29ae3987928a65bab7c196c
Michael Bentz <mbentz@ufl.edu>
2022-04-06 10:08:35 -0400

    Fix REDCap installation spec

--- cypress/tests/finish_redcap_install_spec.ts
+++ cypress/tests/finish_redcap_install_spec.ts
@@ -1,13 +1,23 @@
 describe('Finish REDCap installation', () => {
-    const redcapVersion = Cypress.env('redcap_version');
+  const redcapVersion = Cypress.env('redcap_version');
 
-    it('Install from versioned zip ', () => {
-        cy.visit(Cypress.config().baseUrl);
-        cy.get('div.form-check label.form-check-label').contains("Use a local copy").click();
-        cy.uploadFile(`redcap${redcapVersion}.zip`, "zip", 'input[name=installer-upload]');
-        cy.get('div.form-check label.form-check-label').contains("Prepopulate").click();
-        cy.get('input.supplement-option.form-control').type("admin@ufl.edu");
-          cy.get("button[type='submit']").click();
-        cy.get("h5").contains("Initial setup complete!");
-    })
+  it('Install from versioned zip ', () => {
+    cy.visit(Cypress.config().baseUrl);
+    cy.get('div.form-check label.form-check-label').contains("Use a local copy").click();
+    cy.uploadFile(`redcap${redcapVersion}.zip`, "zip", 'input[name=installer-upload]');
+    cy.get('div.form-check label.form-check-label').contains("Prepopulate").click();
+    cy.get('input.supplement-option.form-control').type("admin@ufl.edu");
+    cy.get("button[type='submit']").click();
+  })
+
+  it('Install completed', () => {
+    const interceptUrl = `${Cypress.config().baseUrl}/*`;
+    const loginUrl = `${Cypress.config().baseUrl}/index.php`;
+
+    cy.intercept('GET', loginUrl).as('loginPage');
+    cy.visit(loginUrl);
+    cy.wait('@loginPage', {requestTimeout: 300000}).then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+    });
+  });
 })
